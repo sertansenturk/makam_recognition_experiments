842159c85d2e15e5c8eeac023b5fa3fa0b8795d3
Sertan Senturk <contact@sertansenturk.com>
2020-06-12 01:46:50 +0100

    MRE-14 Fixed editable package mode

--- docker/jupyter/scripts/run_pytest.sh
+++ docker/jupyter/scripts/run_pytest.sh
@@ -4,3 +4,7 @@ set -e
 ./wait-for-it.sh $1:$2 -t $3 -- \
     echo "Running pytest as $USER with uid $UID" && \
     pytest -vv "$(pwd)/docker"
+
+# test that package is installed in editable mode
+# i.e. if the .egg-link file is in the site-packeges
+ls /opt/conda/lib/python3.7/site-packages/mre.egg-linkas
