da74583d7cdbeda8886bd27e3d7064d7c341418e
Sertan Senturk <contact@sertansenturk.com>
2020-06-20 23:31:10 +0100

    MRE-14 Divided annotation validation into separate methods

--- src/mre/data/annotation.py
+++ src/mre/data/annotation.py
@@ -1,23 +1,30 @@
 import logging
 
+import numpy as np
 import pandas as pd
 
 from ..config import config
 
 logger = logging.Logger(  # pylint: disable-msg=C0103
     __name__, level=logging.INFO)
+cfg = config.read()
 
 
 class Annotation:
     """class to read and process makam recognition annotations"""
     MUSICBRAINZ_RECORDING_URL = "http://musicbrainz.org/recording/"
+    EXPECTED_NUM_RECORDINGS = cfg.getint("dataset", "num_recordings")
+    EXPECTED_NUM_RECORDINGS_PER_MAKAM = cfg.getint(
+        "dataset", "num_recordings_per_makam")
+    EXPECTED_NUM_MAKAMS = (
+        EXPECTED_NUM_RECORDINGS / EXPECTED_NUM_RECORDINGS_PER_MAKAM)
+
+    URL = cfg["dataset"]["annotation_file"]
 
     def __init__(self):
         """instantiates an Annotation object
         """
-        self.url = config.read()["dataset"]["annotation_file"]
-
-        logger.info("Reading annotations from: %s", self.url)
+        logger.info("Reading annotations from: %s", self.URL)
 
         self.data = self._read_from_github()
         self._validate()
@@ -35,74 +42,105 @@ class Annotation:
         """
         return self.data.head()
 
-    def _read_from_github(self):
+    def _read_from_github(self) -> pd.DataFrame:
         """reads the annotation file from github and validates
-        """
-        return pd.read_json(self.url)
-
-    def _get_num_recs_per_makam(self) -> int:
-        """returns number of recordings per makam
 
         Returns
         -------
-        int
-            number of recordings per makam
+        pd.DataFrame
+            annotations
+        """
+        return pd.read_json(self.URL)
+
+    def _validate(self):
+        """runs all validations
+        """
+        self._validate_num_recordings()
+        self._validate_mbids()
+        self._validate_num_recordings_per_makam()
+        self._validate_num_makams()
+
+    def _validate_num_recordings(self):
+        """validates the number of recordings
+
+        Raises
+        ------
+        ValueError
+            if the number of recordings is not equal to the expected
+        """
+        num_recordings = len(self.data)
+        if num_recordings != self.EXPECTED_NUM_RECORDINGS:
+            raise ValueError(
+                f"There are {num_recordings} recordings. "
+                f"Expected: {self.EXPECTED_NUM_RECORDINGS}.")
+        logger.info("%d annotated recordings.", num_recordings)
+
+    def _validate_mbids(self):
+        """validates that all of the MBIDs are non-empty and unique
+
+        Raises
+        ------
+        ValueError
+            raises if MBIDs are not unique
+        """
+        missing_mbid = self.data.mbid.replace("", np.nan).isna()
+        if any(missing_mbid):
+            raise ValueError("Missing MBIDs.")
+
+        unique_num_recordings = self.data.mbid.nunique()
+        num_recordings = len(self.data)
+        if unique_num_recordings != num_recordings:
+            raise ValueError("MBIDs are not unique.")
+        logger.info("%d annotated recordings.", num_recordings)
+
+    def _validate_num_recordings_per_makam(self) -> int:
+        """validates that the number of recordings per makam is equal to
+        expected
+
+        Raises
+        ------
+        ValueError
+            if the dataset is unbalanced (not supported)
+        ValueError
+            if the number of recordings per makams is not equal to the
+            expected
         """
         num_recordings_per_makam = self.data.makam.value_counts()
 
         # balanced dataset; all classes have the same number of instances
         if num_recordings_per_makam.nunique() == 1:
-            return num_recordings_per_makam[0]
+            num_recordings_per_makam = num_recordings_per_makam[0]
+        else:
+            raise ValueError("Unbalanced datasets are not supported.")
 
-        raise ValueError("Inbalanced datasets are not supported")
-
-    def _validate(self):
-        """validates the annotations
-
-        Read the expected values mre config and verifies that:
-            1) number of recordings
-            2) number of makams
-            3) number of recordings per makam
-        have the expected values.
-        """
-        cfg = config.read()
-
-        num_recordings = len(self.data.mbid)
-        expected_num_recs = cfg.getint("dataset", "num_recordings")
-        if num_recordings != expected_num_recs:
-            raise ValueError(
-                f"There are {num_recordings} recordings. "
-                f"Expected: {expected_num_recs}.")
-
-        unique_num_recordings = self.data.mbid.nunique()
-        if unique_num_recordings != num_recordings:
-            raise ValueError("MusicBrainz ID (MBIDs) are not unique.")
-        logger.info("%d annotated recordings.", num_recordings)
-
-        num_recordings_per_makam = self._get_num_recs_per_makam()
-        expected_num_recs_per_makam = cfg.getint(
-            "dataset", "num_recordings_per_makam")
-        if num_recordings_per_makam == expected_num_recs_per_makam:
+        if num_recordings_per_makam != self.EXPECTED_NUM_RECORDINGS_PER_MAKAM:
             raise ValueError(
                 f"{num_recordings_per_makam} number of recordings per makam. "
-                f"Expected: {expected_num_recs_per_makam}")
+                f"Expected: {self.EXPECTED_NUM_RECORDINGS_PER_MAKAM}")
         logger.info("%d recordings per makam.", num_recordings_per_makam)
 
+    def _validate_num_makams(self):
+        """validates that the number of makams is equal to expected
+
+        Raises
+        ------
+        ValueError
+            if the number of makams is not equal to the expected
+        """
         num_makams = len(self.data.makam.value_counts())
-        expected_num_makams = expected_num_recs / expected_num_recs_per_makam
-        if num_makams != expected_num_makams:
+        if num_makams != self.EXPECTED_NUM_MAKAMS:
             raise ValueError(
                 f"There are {num_makams} makams. "
-                f"Expected: {expected_num_makams}.")
+                f"Expected: {self.EXPECTED_NUM_MAKAMS}.")
         logger.info("%d makams.", num_makams)
 
     def _parse_mbid_urls(self):
-        """Parses the urls in the mbid field
+        """Parses the urls in the MBID field
 
         The MBIDs in the "otmm_makam_recognition_dataset" are stored as
         URLs pointing to the MusicBrainz website.
 
-        This method trims mbid's while moving the URL to a new field called
+        This method trims MBIDs while moving the URL to a new field called
         mb_url.
         """
         self.data["mb_url"] = self.data["mbid"]
@@ -117,16 +155,16 @@ class Annotation:
             lambda a: a[-1])
 
     def _patch_dunya_uids(self):
-        """Patches missing dunya uid's with mbid's
+        """Fills missing dunya UIDs with MBIDs
 
         Some recordings in "CompMusic makam music corpus" may not point to the
         master MBID in MusicBrainz due to database merges.
 
         "otmm_makam_recognition_dataset" keeps track of such discrepancies in
         in the dunya_uid field. This field is not populated if the dunya_uid
-        & mbid are the same.
+        & MBID are the same.
 
-        This method fills missing dunya_uid's by the corresponding mbid
+        This method fills missing dunya_uid's by the corresponding MBID
         """
         self.data.loc[self.data["dunya_uid"].isna(), "dunya_uid"] = (
             self.data.loc[self.data["dunya_uid"].isna(), "mbid"])
--- tests/python/data/test_annotation.py
+++ tests/python/data/test_annotation.py
@@ -1,65 +1,138 @@
 import os
 from unittest import mock
 
+import numpy as np
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
 import pytest
 from mre.data import Annotation
+from mre.config import config
 
 
 @pytest.fixture
-def mock_annotation(scope="session"):
-    with mock.patch("mre.data.Annotation.__init__", return_value=None):
-        annotations = Annotation()
-    return annotations
-
-
-
-
-
-# def test_validate():
-#     assert True
+def mock_annotation(scope="session") -> Annotation:
+    with mock.patch("mre.data.Annotation.__init__",
+                    return_value=None,
+                    autospec=True):
+        return Annotation()
 
 
 class TestAnnotation:
-    @mock.patch('pandas.read_json')
+    @mock.patch('pandas.read_json', autospec=True)
     def test_read_from_github(self, mock_read_json, mock_annotation):
+        # GIVEN
+        mock_annotation.URL = "mock_url"
+
         # WHEN
-        annotations = mock_annotation
-        annotations.url = "mock_url"
-        _ = annotations._read_from_github()
+        _ = mock_annotation._read_from_github()
 
         # THEN
-        mock_read_json.assert_called_once_with(annotations.url)
+        mock_read_json.assert_called_once_with(mock_annotation.URL)
 
-    def test_get_num_recs_per_makam_inbalanced_dataset(self, mock_annotation):
+    def test_validate_num_recordings(self, mock_annotation):
         # GIVEN
-        annotations = mock_annotation
-        annotations.data = pd.DataFrame([  # inbalanced
-            {"makam": "makam1"}, {"makam": "makam1"}, {"makam": "makam2"}])
+        mock_annotation.data = pd.DataFrame(
+            [{"col1": "val1"}, {"col1": "val2"}])
+        mock_annotation.EXPECTED_NUM_RECORDINGS = 2
+
+        # WHEN; THEN
+        mock_annotation._validate_num_recordings()
+
+    def test_validate_mbids(self, mock_annotation):
+        # GIVEN
+        mock_annotation.data = pd.DataFrame(
+            [{"mbid": "id1"}, {"mbid": "id2"}])
+
+        # WHEN; THEN
+        mock_annotation._validate_mbids()
+
+    def test_validate_nonunique_mbids(self, mock_annotation):
+        # GIVEN
+        mock_annotation.data = pd.DataFrame(
+            [{"mbid": "id1"}, {"mbid": "id1"}])
 
         # WHEN; THEN
         with pytest.raises(ValueError):
-            annotations._get_num_recs_per_makam()
+            mock_annotation._validate_mbids()
 
+    @pytest.mark.parametrize("data", [
+        pd.DataFrame([{"mbid": ""}]),
+        pd.DataFrame([{"mbid": None}]),
+        pd.DataFrame([{"mbid": np.nan}])])
+    def test_validate_missing_mbid(self, mock_annotation, data):
+        # GIVEN
+        mock_annotation.data = data
+
+        # WHEN; THEN
+        with pytest.raises(ValueError):
+            mock_annotation._validate_mbids()
 
     @pytest.mark.parametrize("data,expected", [
         (pd.DataFrame([{"makam": "makam1"}]), 1),
         (pd.DataFrame([{"makam": "makam1"}, {"makam": "makam2"}]), 1),
         (pd.DataFrame([{"makam": "makam1"}, {"makam": "makam1"},
                        {"makam": "makam2"}, {"makam": "makam2"}]), 2)])
-    def test_get_num_recs_per_makam(self, mock_annotation, data, expected):
+    def test_val_num_recs_per_makam(self, mock_annotation, data, expected):
         # GIVEN
-        annotations = mock_annotation
-        annotations.data = data
+        mock_annotation.data = data
+        mock_annotation.EXPECTED_NUM_RECORDINGS_PER_MAKAM = expected
+
+        # WHEN; THEN
+        mock_annotation._validate_num_recordings_per_makam()
+
+    def test_val_num_recs_per_makam_unbalanced(self, mock_annotation):
+        # GIVEN
+        mock_annotation.data = pd.DataFrame([  # unbalanced
+            {"makam": "makam1"}, {"makam": "makam1"}, {"makam": "makam2"}])
+
+        # WHEN; THEN
+        with pytest.raises(ValueError):
+            mock_annotation._validate_num_recordings_per_makam()
+
+    def test_val_num_recs_per_makam_unexpected(self, mock_annotation):
+        # GIVEN
+        mock_annotation.data = pd.DataFrame([  # 2 recordings per makams
+            {"makam": "makam1"}, {"makam": "makam2"}])
+        mock_annotation.EXPECTED_NUM_RECORDINGS_PER_MAKAM = 5  # not 2
+
+        # WHEN; THEN
+        with pytest.raises(ValueError):
+            mock_annotation._validate_num_recordings_per_makam()
+
+    @pytest.mark.parametrize("data,expected", [
+        (pd.DataFrame([{"makam": "makam1"}]), 1),
+        (pd.DataFrame([{"makam": "makam1"}, {"makam": "makam2"}]), 2)])
+    def test_validate_num_makams(self, mock_annotation, data, expected):
+        # GIVEN
+        mock_annotation.data = data
+        mock_annotation.EXPECTED_NUM_MAKAMS = expected
+
+        # WHEN; THEN
+        mock_annotation._validate_num_makams()
+
+    def test_validate_num_makams_incorrect(self, mock_annotation):
+        # GIVEN
+        mock_annotation.data = pd.DataFrame([  # 3 makams
+            {"makam": "makam1"}, {"makam": "makam2"}, {"makam": "makam3"}])
+        mock_annotation.EXPECTED_NUM_MAKAMS = 5  # not 2
+
+        # WHEN; THEN
+        with pytest.raises(ValueError):
+            mock_annotation._validate_num_makams()
+
+    def test_parse_mbid_urls(self, mock_annotation):
+        # GIVEN
+        mb_url = "http://musicbrainz.org/recording/some_mbid"
+        mock_annotation.data = pd.DataFrame([{"mbid": mb_url}])
 
         # WHEN
-        result = annotations._get_num_recs_per_makam()
+        mock_annotation._parse_mbid_urls()
 
         # THEN
-        assert result == expected
+        expected = pd.DataFrame([{"mb_url": mb_url, "mbid": "some_mbid"}])
 
+        assert_frame_equal(mock_annotation.data, expected, check_like=True)
 
     @pytest.mark.parametrize("invalid_url", [
         None,
@@ -71,26 +144,11 @@ class TestAnnotation:
     ])
     def test_parse_mbid_urls_invalid_url(self, mock_annotation, invalid_url):
         # GIVEN
-        annotations = mock_annotation
-        annotations.data = pd.DataFrame([{"mbid": invalid_url}])
+        mock_annotation.data = pd.DataFrame([{"mbid": invalid_url}])
 
         # WHEN; THEN
         with pytest.raises(ValueError):
-            annotations._parse_mbid_urls()
-
-    def test_parse_mbid_urls(self, mock_annotation):
-        # GIVEN
-        annotations = mock_annotation
-        mb_url = "http://musicbrainz.org/recording/some_mbid"
-        annotations.data = pd.DataFrame([{"mbid": mb_url}])
-
-        # WHEN
-        annotations._parse_mbid_urls()
-
-        # THEN
-        expected = pd.DataFrame([{"mb_url": mb_url, "mbid": "some_mbid"}])
-
-        assert_frame_equal(annotations.data, expected, check_like=True)
+            mock_annotation._parse_mbid_urls()
 
     @pytest.mark.parametrize("data,expected", [
         (pd.DataFrame([{"dunya_uid": None, "mbid": "some_mbid"}]),
@@ -101,11 +159,10 @@ class TestAnnotation:
          pd.DataFrame([{"dunya_uid": "some_uid", "mbid": None}]))])
     def test_patch_dunya_uids(self, mock_annotation, data, expected):
         # GIVEN
-        annotations = mock_annotation
-        annotations.data = data
+        mock_annotation.data = data
 
         # WHEN
-        annotations._patch_dunya_uids()
+        mock_annotation._patch_dunya_uids()
 
         # THEN
-        assert_frame_equal(annotations.data, expected, check_like=True)
+        assert_frame_equal(mock_annotation.data, expected, check_like=True)
